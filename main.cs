93b097517bfe1ee57c62eab3953102aec3493855
Risky <131745729+RyuXyro@users.noreply.github.com>
2024-06-21 12:45:01 +0700

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Belajar
+{
+    public static void Main(string[] args)
+    {
+        string abc = "Kazuo";
+        int def = 12;
+        Console.WriteLine("tulis sesuatu");
+        Console.WriteLine($"{Console.ReadLine()}, {abc} ultah tanggal {def}");
+    }
+}
